cdc5beb04e775ac24d49388fb5f11e31323b6f0b
Daniel <dgplays14@gmail.com>
2023-07-16 00:23:37 -0300

    Add README.md

---
+++ README.md
@@ -0,0 +1,41 @@
+# 🎮 Rock-paper-scissors Game
+
+![](./assets/images/app-screenshot.png)
+
+## 📄 Descrição
+
+Este projeto é uma solução para o desafio Rock-paper-scissors Game do site Frontend Mentor :D
+
+É um simples jogo de Pedra, Papel e Tesoura implementado utilizando HTML, CSS e JavaScript. Permite que os jogadores joguem contra o computador e mantém o controle do placar.
+
+Link da projeto: https://grazziotti.github.io/rock-paper-scissors-game/
+
+## 🛠 Tecnologias Utilizadas
+
+- HTML5
+- CSS3
+- JavaScript
+
+## 📋 Pré-requisitos
+
+Antes de começar, você vai precisar ter instalado em sua máquina as seguintes ferramentas: [Git](https://git-scm.com/).
+
+Além disto é bom ter um editor para trabalhar com o código como [VSCode](https://code.visualstudio.com/).
+
+## 🚀 Como Executar
+
+```bash
+# Clone este repositório
+$ git clone https://github.com/grazziotti/rock-paper-scissors-game
+
+# Navegue até o diretório do projeto.
+
+# Execute o projeto
+Abra o arquivo index.html no seu navegador.
+```
+
+## 👤 Autor
+
+<img style="border-radius: 50px" alt="Daniel Grazziotti" title="Daniel Grazziotti" src="https://avatars.githubusercontent.com/grazziotti" height="100" width="100" />
+
+[Daniel Grazziotti](https://github.com/grazziotti)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,32 @@
 // modal variables
-const modal = document.querySelector('.modal-fullscreen')
-const rulesBtn = document.querySelector('.rules-btn') 
-const closeModalBtn = document.querySelector('.modal__container__close-btn')
+const modal = document.querySelector(".modal-fullscreen");
+const rulesBtn = document.querySelector(".rules-btn");
+const closeModalBtn = document.querySelector(".modal__container__close-btn");
 
 // game variables
-const gameContainer = document.querySelector('.game')
-let score = 0
+const gameContainer = document.querySelector(".game");
+let score = 0;
 
-
-
-// modal 
+// modal
 const closeModal = () => {
-    modal.classList.remove('modal--show')
-    setTimeout( () => modal.classList.remove('modal--open'), 500)
-}
-closeModalBtn.addEventListener('click', closeModal)
+  modal.classList.remove("modal--show");
+  setTimeout(() => modal.classList.remove("modal--open"), 500);
+};
+closeModalBtn.addEventListener("click", closeModal);
 
 const openModal = () => {
-    modal.classList.add('modal--open')
-    setTimeout( () => modal.classList.add('modal--show'), 20)
-}
-rulesBtn.addEventListener('click', openModal)
-
-
+  modal.classList.add("modal--open");
+  setTimeout(() => modal.classList.add("modal--show"), 20);
+};
+rulesBtn.addEventListener("click", openModal);
 
 // game
 const showPickingScreen = () => {
-    const div = document.createElement('div')
-    div.classList.add('picking-container')
-    div.setAttribute('data-anime', 'top')
-  
-    div.innerHTML = `
+  const div = document.createElement("div");
+  div.classList.add("picking-container");
+  div.setAttribute("data-anime", "top");
+
+  div.innerHTML = `
         <div data-picking="paper" class="picking picking--paper">
             <div class="picking__img-container">
                 <img src="./assets/images/icon-paper.svg" alt="Picking image">
@@ -46,149 +42,154 @@ const showPickingScreen = () => {
                 <img src="./assets/images/icon-rock.svg" alt="Picking image">
             </div>
         </div>
-    `
+    `;
 
-    setTimeout( () => div.classList.add('animate'), 10)
+  setTimeout(() => div.classList.add("animate"), 10);
 
-    gameContainer.appendChild(div)
-}
+  gameContainer.appendChild(div);
+};
 
-const clearGameScreen = () => gameContainer.innerHTML = ''
+const clearGameScreen = () => (gameContainer.innerHTML = "");
 
 const init = () => {
-    clearGameScreen()
-    showPickingScreen()
-    
-    const gamePickings = document.querySelectorAll('.picking')
+  clearGameScreen();
+  showPickingScreen();
 
-    gamePickings.forEach( gameOption => {
-        gameOption.addEventListener('click', game)
-    })
+  const gamePickings = document.querySelectorAll(".picking");
 
-}
+  gamePickings.forEach((gameOption) => {
+    gameOption.addEventListener("click", game);
+  });
+};
 
-const updateScore = result => {
-    const scoreBoard = document.querySelector('.score__board__number')
-    
-    switch (result) {
-        case 'you win':
-            score++
-            break
-        case 'you lose':
-            score--
-            break
-    }
-    
-    scoreBoard.innerText = score
-}
+const updateScore = (result) => {
+  const scoreBoard = document.querySelector(".score__board__number");
 
-const showResult = result => {
-    const resultContainer = document.createElement('div')
-    resultContainer.classList.add('result')
-    resultContainer.innerHTML = `
+  switch (result) {
+    case "you win":
+      score++;
+      break;
+    case "you lose":
+      score--;
+      break;
+  }
+
+  scoreBoard.innerText = score;
+};
+
+const showResult = (result) => {
+  const resultContainer = document.createElement("div");
+  resultContainer.classList.add("result");
+  resultContainer.innerHTML = `
         <h1 class="result__msg">${result}</h1>
         <button class="result__btn">play again</button>
-    `
+    `;
 
-    const playAgainBtn = resultContainer.querySelector('.result__btn')
-    playAgainBtn.addEventListener('click', init)
+  const playAgainBtn = resultContainer.querySelector(".result__btn");
+  playAgainBtn.addEventListener("click", init);
 
-    gameContainer.appendChild(resultContainer)
+  gameContainer.appendChild(resultContainer);
 
-    setTimeout(() => resultContainer.classList.add('result--show'), 20)
+  setTimeout(() => resultContainer.classList.add("result--show"), 20);
 
-    const comparisonContainer = document.querySelector('.comparison')
-    comparisonContainer.classList.add('comparison--result')
+  const comparisonContainer = document.querySelector(".comparison");
+  comparisonContainer.classList.add("comparison--result");
 
-    switch (result) {
-        case 'you win':
-            const playerPickContainer = document.querySelector('.comparison__pick:first-child .picking')
-            playerPickContainer.classList.add('picking--winner')
-            break
-        case 'you lose':
-            setTimeout( () => {
-                const computerPickContainer = document.querySelector('.comparison__pick:last-child .picking')
-                computerPickContainer.classList.add('picking--winner')
-            }, 20)
-            break
-    }
-}
+  switch (result) {
+    case "you win":
+      const playerPickContainer = document.querySelector(
+        ".comparison__pick:first-child .picking"
+      );
+      playerPickContainer.classList.add("picking--winner");
+      break;
+    case "you lose":
+      setTimeout(() => {
+        const computerPickContainer = document.querySelector(
+          ".comparison__pick:last-child .picking"
+        );
+        computerPickContainer.classList.add("picking--winner");
+      }, 20);
+      break;
+  }
+};
 
 const getResult = (playerPick, computerPick) => {
-    if (playerPick === computerPick) return 'tie'
+  if (playerPick === computerPick) return "tie";
 
-    if (
-        playerPick === 'paper' && computerPick === 'rock' || 
-        playerPick === 'scissors' && computerPick === 'paper' ||  
-        playerPick === 'rock' && computerPick === 'scissors') 
-    {
-        return 'you win'
-    } else {
-        return 'you lose'
-    }
-}
+  if (
+    (playerPick === "paper" && computerPick === "rock") ||
+    (playerPick === "scissors" && computerPick === "paper") ||
+    (playerPick === "rock" && computerPick === "scissors")
+  ) {
+    return "you win";
+  } else {
+    return "you lose";
+  }
+};
 
-const showComputerPick = computerPick => {
-    const computerPickContainer = document.querySelector('.picking--loading')
-    computerPickContainer.classList.remove('picking--loading')
-    computerPickContainer.classList.add(`picking--${computerPick}`)
-    
-    const computerPickContainerImg = computerPickContainer.querySelector('.picking__img-container img')
-    computerPickContainerImg.src = `./assets/images/icon-${computerPick}.svg`
-    computerPickContainerImg.alt = 'Picking image'
-}
+const showComputerPick = (computerPick) => {
+  const computerPickContainer = document.querySelector(".picking--loading");
+  computerPickContainer.classList.remove("picking--loading");
+  computerPickContainer.classList.add(`picking--${computerPick}`);
+
+  const computerPickContainerImg = computerPickContainer.querySelector(
+    ".picking__img-container img"
+  );
+  computerPickContainerImg.src = `./assets/images/icon-${computerPick}.svg`;
+  computerPickContainerImg.alt = "Picking image";
+};
 
 const getComputerPick = () => {
-    const gamePickings = ['paper', 'rock', 'scissors', '']
-    const index = Math.floor(Math.random() * 3)
-    return gamePickings[index]
-}
+  const gamePickings = ["paper", "rock", "scissors", ""];
+  const index = Math.floor(Math.random() * 3);
+  return gamePickings[index];
+};
 
-const showComparisonScreen = playerPick => {
-    const comparisonContainer = document.createElement('div')
-    comparisonContainer.classList.add('comparison')
-    
-    const playerPickContainer = document.createElement('div')
-    playerPickContainer.classList.add('comparison__pick')
-    playerPickContainer.innerHTML = `
+const showComparisonScreen = (playerPick) => {
+  const comparisonContainer = document.createElement("div");
+  comparisonContainer.classList.add("comparison");
+
+  const playerPickContainer = document.createElement("div");
+  playerPickContainer.classList.add("comparison__pick");
+  playerPickContainer.innerHTML = `
         <p class="comparison__pick__heading">you picked</p>
         <div class="picking picking--${playerPick}">
             <div class="picking__img-container">
                 <img src="./assets/images/icon-${playerPick}.svg" alt="Picking image">
             </div>
         </div>
-    `
+    `;
 
-    const computerPickLoading = document.createElement('div')
-    computerPickLoading.classList.add('comparison__pick')
-    computerPickLoading.innerHTML = `
+  const computerPickLoading = document.createElement("div");
+  computerPickLoading.classList.add("comparison__pick");
+  computerPickLoading.innerHTML = `
         <p class="comparison__pick__heading">the house picked</p>
         <div class="picking picking--loading">
             <div class="picking__img-container">
                 <img src="" alt="">
             </div>
         </div>
-    `
+    `;
 
-    comparisonContainer.appendChild(playerPickContainer)
-    comparisonContainer.appendChild(computerPickLoading)
+  comparisonContainer.appendChild(playerPickContainer);
+  comparisonContainer.appendChild(computerPickLoading);
 
-    gameContainer.appendChild(comparisonContainer)
-}
+  gameContainer.appendChild(comparisonContainer);
+};
 
-const game = e => {
-    const playerPick = e.target.closest('.picking').getAttribute('data-picking')
-    const computerPick = getComputerPick()
-    const result = getResult(playerPick, computerPick)
+const game = (e) => {
+  const playerPick = e.target.closest(".picking").getAttribute("data-picking");
+  const computerPick = getComputerPick();
+  const result = getResult(playerPick, computerPick);
 
-    clearGameScreen()
-    showComparisonScreen(playerPick)
+  clearGameScreen();
+  showComparisonScreen(playerPick);
 
-    setTimeout(() => {
-        showComputerPick(computerPick)
-        showResult(result)
-        updateScore(result)
-    }, 2000)
-}
+  setTimeout(() => {
+    showComputerPick(computerPick);
+    showResult(result);
+    updateScore(result);
+  }, 2000);
+};
 
-init()
+init();
